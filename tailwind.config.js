cfdc89782db8dc8df7cb6d28b4530f7692cfed06
anjali <anju.sahu2003@gmail.com>
2023-12-24 18:25:52 +0530

    pixelsfilm

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,19 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        wiggle: 'fade 1s ease-in-out infinite',
+        wiggle: "fade 1s ease-in-out infinite",
       },
-      colors:{
-        'Off Black Hex':'#1a1b1f',
-        'Pale Yellow Hex':'#f0ece7'
+      colors: {
+        "Off Black Hex": "#1a1b1f",
+        "Pale Yellow Hex": "#f0ece7",
       },
       fontFamily: {
-        'sans': ['Montserrat','sans-serif' ],
-        'serif': [ 'DM Serif Display','serif' ],
- 
+        sans: ["Montserrat", "sans-serif"],
+        serif: ["DM Serif Display", "serif"],
       },
     },
   },
   plugins: [],
-}
+};
 
-// npx tailwindcss -i ./docs/input.css -o ./docs/output.css --watch
+// npx tailwindcss -i ../input.css -o ../output.css --watch
